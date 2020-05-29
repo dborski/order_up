127b4fa40b8e56e7b7da2a4602c203006011409d
Derek Borski <derek.borski@gmail.com>
2020-05-29 09:40:49 -0600

    Complete US 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+    @chef1 = Chef.create!(name: "Chef Boyardee")
+    @dish1 = @chef1.dishes.create!(name: "Ravioli", description: "Not as good as you remember")
+
+    @ingrdient1 = Ingredient.create!(name: "Beef", calories: 100)
+    @ingrdient2 = Ingredient.create!(name: "Cheese", calories: 50)
+    @ingrdient3 = Ingredient.create!(name: "Ravioli shells", calories: 150)
+
+    DishIngredient.create!(dish: @dish1, ingredient: @ingrdient1)
+    DishIngredient.create!(dish: @dish1, ingredient: @ingrdient2)
+    DishIngredient.create!(dish: @dish1, ingredient: @ingrdient3)
---
+++ spec/features/dish_show_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe 'When a user visits a dish show page', type: :feature do
+  before(:each) do
+    @chef1 = Chef.create!(name: "Chef Boyardee")
+    @dish1 = @chef1.dishes.create!(name: "Ravioli", description: "Not as good as you remember")
+
+    @ingrdient1 = Ingredient.create!(name: "Beef", calories: 100)
+    @ingrdient2 = Ingredient.create!(name: "Cheese", calories: 50)
+    @ingrdient3 = Ingredient.create!(name: "Ravioli shells", calories: 150)
+
+    DishIngredient.create!(dish: @dish1, ingredient: @ingrdient1)
+    DishIngredient.create!(dish: @dish1, ingredient: @ingrdient2)
+    DishIngredient.create!(dish: @dish1, ingredient: @ingrdient3)
+
+    visit dish_path(@dish1)
+  end
+  it 'shows list of ingredients for that dish and the chefs name' do
+
+    expect(page).to have_content(@dish1.name)
+    expect(page).to have_content(@ingrdient1.name)
+    expect(page).to have_content(@ingrdient2.name)
+    expect(page).to have_content(@ingrdient3.name)
+    expect(page).to have_content(@chef1.name)
+  end
+end 
